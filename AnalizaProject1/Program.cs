e0c50840099e55ac2782fa31a68ea1d4b351ada3
nahmanbo <nhmanbo2003@gmail.com>
2025-05-12 15:51:49 +0300

    add 3 func of valid input

--- AnalizaProject1/Program.cs
+++ AnalizaProject1/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿namespace AnalizaProject1;
+
+class Program
+{
+    bool isInputAvailable(string[] input)
+    {
+        return input.Length > 0;
+    }
+
+    bool isAllNumbers(string[] input)
+    {
+        foreach (string item in input)
+            if (!item.All(char.IsDigit))
+                return false;    
+        return true;
+    }
+
+    bool isThreePositiveNum(string[] input)
+    {
+        int counter = 0;
+        foreach (string item in input)
+            if(Convert.ToInt32(item) > 0)
+                counter++;
+            if(counter == 3)
+                return true;
+        return false;
+    }
+    
+    static void Main(string[] args)
+    {
+    }
+}
